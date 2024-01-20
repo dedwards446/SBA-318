750a12d3a4289194bfdb45567f621df372e494a1
dedwards446 <150183824+dedwards446@users.noreply.github.com>
2024-01-19 19:04:14 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Custom Middleware 1
+app.use((req, res, next) => {
+  console.log('Custom Middleware 1');
+  // Your middleware logic here
+  next();
+});
+
+// Custom Middleware 2
+app.use((req, res, next) => {
+  console.log('Custom Middleware 2');
+  // Your middleware logic here
+  next();
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// In-memory data
+let dataCategory = [];
+
+// Routes
+app.get('/', (req, res) => {
+  // Render index view with current state of data
+  res.render('index', { data: dataCategory });
+});
+
+app.get('/form', (req, res) => {
+  // Render form view for interaction
+  res.render('form');
+});
+
+app.post('/api/create', (req, res) => {
+  // Handle POST request to create data
+  const newData = req.body.data;
+  dataCategory.push(newData);
+  res.redirect('/');
+});
+
+// Set up static files (CSS, images)
+app.use(express.static(__dirname + '/public'));
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
